7a2dd1d175ad33c71cb2d74ebabe8d81b3007581
Daniel <marcosdanielsferreira2015@gmail.com>
2026-08-03 09:36:53 -0300

    Criação de menu principal

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Fabrica
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== FABRICA ===");
+
+            Console.WriteLine("1 - Cadastrar produto");
+            Console.WriteLine("2 - Listar produtos");
+            Console.WriteLine("3 - Atualizar produto");
+            Console.WriteLine("4 - Remover produto");
+            
+            Console.WriteLine("5 - Cadastrar matéria-prima");
+            Console.WriteLine("6 - Listar máteria-prima");
+            Console.WriteLine("7 - Atualizar matéria-prima");
+
+            Console.WriteLine("8 - Criar receita de produção");
+            Console.WriteLine("9 - Criar pedido de produção");
+
+            Console.WriteLine("10 - Ver pedidos");
+            Console.WriteLine("11 - Verificar se pedido pode ser produzido");
+            Console.WriteLine("12 - Iniciar produção");
+            Console.WriteLine("13 - Finalizar produção");
+
+            Console.WriteLine("14 - Relatórios");
+            
+            Console.WriteLine("0 - Sair");
+        }
+    }
+}
