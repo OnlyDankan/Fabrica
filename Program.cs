1d664fa45cd9b8281afbc9062ab3b4879c882281
Daniel <marcosdanielsferreira2015@gmail.com>
2026-08-03 09:42:40 -0300

    criando separação de linhas

--- Program.cs
+++ Program.cs
@@ -13,21 +13,21 @@ namespace Fabrica
             Console.WriteLine("3 - Atualizar produto");
             Console.WriteLine("4 - Remover produto");
             
-            Console.WriteLine("5 - Cadastrar matéria-prima");
+            Console.WriteLine("\n5 - Cadastrar matéria-prima");
             Console.WriteLine("6 - Listar máteria-prima");
             Console.WriteLine("7 - Atualizar matéria-prima");
 
-            Console.WriteLine("8 - Criar receita de produção");
+            Console.WriteLine("\n8 - Criar receita de produção");
             Console.WriteLine("9 - Criar pedido de produção");
 
-            Console.WriteLine("10 - Ver pedidos");
+            Console.WriteLine("\n10 - Ver pedidos");
             Console.WriteLine("11 - Verificar se pedido pode ser produzido");
             Console.WriteLine("12 - Iniciar produção");
             Console.WriteLine("13 - Finalizar produção");
 
-            Console.WriteLine("14 - Relatórios");
+            Console.WriteLine("\n14 - Relatórios");
             
-            Console.WriteLine("0 - Sair");
+            Console.WriteLine("\n0 - Sair");
         }
     }
 }
