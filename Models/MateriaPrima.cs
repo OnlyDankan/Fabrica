91ae9335f930aa4686e5cafa4d05a00698da31a9
Daniel <marcosdanielsferreira2015@gmail.com>
2026-08-04 10:33:26 -0300

    Criação da classe materia prima

---
+++ Models/MateriaPrima.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Dynamic;
+
+namespace Fabrica.Models
+{
+    public class MateriaPrima
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
