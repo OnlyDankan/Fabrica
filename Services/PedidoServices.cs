3e79b3f4280060ccd4583648e5f3198930e2c1f1
Daniel <marcosdanielsferreira2015@gmail.com>
2026-08-17 15:00:14 -0300

    Update PedidoServices.cs

---
+++ Services/PedidoServices.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
+using System.Data;
+using Fabrica.Models;
+using System.Diagnostics;
+using Fabrica.Services;
+
+
+namespace Pedido.Services
+{
+    public class PedidoService
+    {
+        static void Main(string[] args)
+        {
+             FabricaService fabricaService = new FabricaService();
+        }
+    }
+}
