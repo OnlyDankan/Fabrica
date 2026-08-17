a695cafd25550cc717bcbe8cb322c42dc0573b6d
Daniel <marcosdanielsferreira2015@gmail.com>
2026-08-17 14:50:36 -0300

    Criação de PedidoService

--- obj/Debug/net10.0/Fabrica.AssemblyInfo.cs
+++ obj/Debug/net10.0/Fabrica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fabrica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb27ec840b461a9351645fc70dce219d4ac783e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d41f3bfba7f1a97d04561ab6d03c49570350a00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fabrica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fabrica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
