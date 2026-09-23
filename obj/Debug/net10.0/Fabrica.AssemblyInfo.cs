9cea665f9d8f0f8a422975c4f8553eb445d2c563
Daniel <marcosdanielsferreira2015@gmail.com>
2026-09-23 19:34:21 -0300

    assembly

--- obj/Debug/net10.0/Fabrica.AssemblyInfo.cs
+++ obj/Debug/net10.0/Fabrica.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fabrica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+550f16e62b25966f331baf963a4301fbaf841b74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+115de03b8122e668f9185ac70ee88e796e9bdfc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fabrica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fabrica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
